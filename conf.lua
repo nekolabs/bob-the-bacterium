715aac8675b95771b82454fc71f260bbb63c4906
Gaichu <gaichu@server.fake>
2014-01-17 14:12:21 +0100

    0.9.0 ready

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t) 
+ 	t.window.width   = 800
+	t.window.height  = 600
+	t.author         = "jam"
+	t.title          = "Bob The Bacterium"
+	t.version		     = "0.9.0"
+	t.console        = false 
+  t.window.vsync 	 = false
+end
