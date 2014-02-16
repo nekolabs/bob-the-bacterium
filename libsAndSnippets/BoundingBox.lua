3a34314b1e0cf63e249f82cbf62acd165b73da8d
Gaichu <gaichu@server.fake>
2014-02-16 01:52:35 +0100

    rm unncessary folders and rest of files

--- libsAndSnippets/BoundingBox.lua
+++
@@ -1,6 +0,0 @@
--- BoundingBox.lua 
--- The point of the snippet is that two boxes can detect each other
-function CheckCollision(ax1,ay1,aw,ah, bx1,by1,bw,bh)
-  local ax2,ay2,bx2,by2 = ax1 + aw, ay1 + ah, bx1 + bw, by1 + bh
-  return ax1 < bx2 and ax2 > bx1 and ay1 < by2 and ay2 > by1
-end
